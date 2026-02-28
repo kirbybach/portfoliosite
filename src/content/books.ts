6942b24be39f4e46b0c6cdd6aa886591283d6fa0
Kirby Bach <kirby.bach1@gmail.com>
2026-02-28 15:54:04 -0500

    added book thoughts

--- src/content/books.ts
+++ src/content/books.ts
@@ -19,23 +19,27 @@ export const books: Book[] = [
         author: 'Andy Weir',
         cover: '/images/the-martian-cover.jpg',
         status: 'finished',
+        thoughts: 'Mark Watney is so tough. Super intense read, couldn\'t put it down.',
     },
     {
         title: 'Project Hail Mary',
         author: 'Andy Weir',
         cover: '/images/project-hail-mary-cover.jpg',
         status: 'finished',
+        thoughts: 'Read this before The Martian. What a great book. Super cool sci fi and just great characters.'
     },
     {
         title: 'Atomic Habits',
         author: 'James Clear',
         cover: '/images/atomic-habits-cover.jpg',
         status: 'finished',
+        thoughts: 'Probably the most impactful book I\'ve read, super practical and applicable to life.',
     },
     {
         title: 'The Almanack of Naval Ravikant',
         author: 'Eric Jorgenson',
         cover: '/images/navalmanack-cover.jpg',
         status: 'finished',
+        thoughts: 'Listened to this one as an audiobook. I liked it, had some good wisdom, the part about being present especially.'
     },
 ]
